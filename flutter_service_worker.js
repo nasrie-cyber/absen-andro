6805725db8c9e3e839971cb4e40aaa98c3e7c5a0
Muhammad Nasri <75401405+nasrie-cyber@users.noreply.github.com>
2021-07-19 15:34:17 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,24 +3,27 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "6591ecad09e551183def96faee3aa23a",
+  "assets/AssetManifest.json": "2ab48451e158ecedfbc2423641982f1d",
 "assets/assets/bg.jpg": "f13ec4cc46a4d0db2519b56cdc3996f4",
+"assets/assets/bg.webp": "a10ae48d79d1c38335a5d9bd1285846f",
 "assets/assets/bg2.jpg": "001d7856a88032893b730f1668ca2c18",
+"assets/assets/bg2.webp": "36225aee7881f51f525cb7ef0084c797",
 "assets/assets/ios.jpg": "0a6d68a0d7b50a12066eb217d3ecbf96",
+"assets/assets/ios.webp": "819a0bfa90a1bf24f35cab1cd6f9aaf9",
 "assets/assets/iphone.png": "1589692714ebe9ed6ef1a0adfd34ecd5",
 "assets/assets/iphonex.png": "2d7427f08fb4614ef4b3b2e7213d7414",
 "assets/assets/korpri.jpg": "279eb46142442279e415e136d6dcb34b",
 "assets/FontManifest.json": "1b1e7812d9eb9f666db8444d7dde1b20",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "7ccbfe837dae9008afb64a491c0cf4e1",
+"assets/NOTICES": "b0bf9c068cdcc660023a0d0eb4b67aa7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "174c02fc4609e8fc4389f5d21f16a296",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5143156e6490bd5474de0a7e53af5ed2",
-"/": "5143156e6490bd5474de0a7e53af5ed2",
-"main.dart.js": "628372294db5c7d22b070e49341b096d",
+"index.html": "9589651d1f78123295b664ef77ee0aaf",
+"/": "9589651d1f78123295b664ef77ee0aaf",
+"main.dart.js": "5bf4b20837f974a0dbc9353565af35af",
 "manifest.json": "de2fa6c0b07304750e669367ca3d2277",
 "version.json": "790756534e7e5776686bc99fe1a5e257"
 };
